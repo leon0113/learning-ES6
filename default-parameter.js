1baca7485d66db3d2795173d19bc7f89ea52a3c4
leon0113 <tahjib.leon399@gmail.com>
2022-09-17 05:33:40 +0600

    default parameter

---
+++ default-parameter.js
@@ -0,0 +1,14 @@
+//adding default value to parameter
+function add(num1 = 0, num2 = 0) {
+    const total = num1 + num2;
+    return total;
+}
+const result = add(2);
+console.log(result);
+
+function fullName(first, last = 'Warning!!!NO last name added') {
+    const name = first + ' ' + last;
+    return name;
+}
+const person = fullName('Leon',)
+console.log(person);
