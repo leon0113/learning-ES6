c36995409eaf680fe2e2ccdb9a8029824cb175d4
leon0113 <tahjib.leon399@gmail.com>
2022-09-16 19:18:22 +0600

    recap

--- recap.js
+++ recap.js
@@ -1,44 +1,19 @@
-#include < stdio.h >
-    #include < conio.h >
+//Arry 
+const numbers = [40, 50, 3, 12, 69, 85, 25, 47];
+const count = numbers.length;
+numbers[2] = 6;
+console.log(numbers);
 
-    void main()
-{
-    int i = 0, flag = 0;
-    char keyword[10][10] = { "int", "float", "long", "string", "if", "else", "for", "while", "do", "break", "default"}, a[10];
-    // input from the user
-    printf("Enter the identifier:");
-    gets(a);
+console.log(count);
 
-
-    for (i = 0; i < 10; i++) {
-        if ((strcmp(keyword[i], a) == 0)) {
-            flag = 1;
-        }
-    }
-
-
-    if (flag == 1) {
-        printf("%s is a keyword", a);
-    }
-    else
-        flag = 0;
-
-    if ((a[0] == "_") || (isalpha(a[0]) != 0)) {
-        for (i = 1; a[i] != '\0'; i++) {
-            if ((isalnum(a[i]) == 0) && (a[i] != '_')) {
-                flag = 1;
-            }
-        }
-    }
-    else {
-        flag = 1;
-    }
-
-    if (flag == 0) {
-        printf("\n %s is an identifier.", a);
-    }
-    else {
-        printf("\n %s is not a valid identifier", a);
-    }
-    return 0;
+// function
+function fullName(first, second) {
+    const name = first + " " + second;
+    return name;
 }
+const person = fullName('Tahjib', 'Leon');
+console.log(person);
+
+// Object
+const bottle = { color: 'red', contains: 'water', price: 150 };
+console.log(bottle);
